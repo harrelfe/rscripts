cfc0de7432338fb573f734330586ad57bd8fac26
harrelfe <f.harrell@vanderbilt.edu>
2018-11-22 09:54:17 -0600

    ssmtp: non-symlinked version

---
+++ ssmtp.r
@@ -0,0 +1,23 @@
+
+## Instead try https://help.ubuntu.com/community/EmailAlerts
+## Also see https://wiki.archlinux.org/index.php/SSMTP
+## sudo apt-get install ssmtp
+## sudo cp ssmtp.conf /etc/ssmtp/
+## sudo cp revaliases /etc/ssmtp/
+## echo -e "To: Frank Harrell <harrelfe@gmail.com>\nSubject: Test Subject\n\nBody 1\nBody 2\nBody 3" | ssmtp -vvv harrelfe@gmail.com
+
+ssmtp <- function(to, subject, body, verbose=FALSE) {
+    x <- strsplit(to, '<')[[1]]
+    toname <- trimws(x[1])
+    toaddr <- sub('>', '', trimws(x[2]))
+    tf <- tempfile()
+    cat('To: ', to, '\n', file=tf, sep='')
+    cat('Subject: ', subject, '\n\n', sep='', file=tf, append=TRUE)
+    cat(body, '\n', sep='', file=tf, append=TRUE)
+    cmd <- paste('ssmtp', if(verbose) '-vvv', toaddr, '<', tf)
+    system(cmd)
+    invisible()
+}
+body <- 'This is a body of an email message\nLine 2\nLine 3'
+ssmtp('Frank Harrell <harrelfe@gmail.com>', 'Test Subject Line', body)
+
