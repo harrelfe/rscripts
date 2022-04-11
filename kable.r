67620579f69316d904b24f0522d019c31b15a775
harrelfe <fh@fharrell.com>
2022-04-11 06:53:39 -0500

    kabl: addedd col.names argument

--- kable.r
+++ kable.r
@@ -4,13 +4,14 @@
 ## digits applies equally to all tables
 ## caption applies to the overall result
 
-kabl <- function(..., caption=NULL, digits=4) {
+kabl <- function(..., caption=NULL, digits=4, col.names=NA) {
   w <- list(...)
   tr <- function(x)
     if(is.vector(x) || (is.array(x) && length(dim(x)) == 1)) t(x) else x
   if(length(w) == 1) {
     w <- w[[1]]
-    return( knitr::kable(tr(w), digits=digits, caption=caption))
+    return( knitr::kable(tr(w), digits=digits, caption=caption,
+                         col.names=col.names))
   }
   w <- lapply(w, function(x) knitr::kable(tr(x), digits=digits))
   knitr::kables(w, caption=caption)
