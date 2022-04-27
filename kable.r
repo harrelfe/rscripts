6ff5b14d7669250c2134f167d6b61608fc252050
harrelfe <fh@fharrell.com>
2022-04-27 10:20:23 -0500

    kabl: sense output format from knitr

--- kable.r
+++ kable.r
@@ -8,11 +8,13 @@ kabl <- function(..., caption=NULL, digits=4, col.names=NA, row.names=NA) {
   w <- list(...)
   tr <- function(x)
     if(is.vector(x) || (is.array(x) && length(dim(x)) == 1)) t(x) else x
+  format <- if(knitr::is_latex_format()) 'latex' else 'pipe'
   if(length(w) == 1) {
     w <- w[[1]]
     return( knitr::kable(tr(w), digits=digits, caption=caption,
-                         col.names=col.names, row.names=row.names))
+                         col.names=col.names, row.names=row.names,
+                         format=format))
   }
-  w <- lapply(w, function(x) knitr::kable(tr(x), digits=digits))
-  knitr::kables(w, caption=caption)
+  w <- lapply(w, function(x) knitr::kable(tr(x), digits=digits, format=format))
+  knitr::kables(w, caption=caption, format=format)
 }
