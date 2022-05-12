3c6b297d2058f34eade9f9f7e249f39e7186fa9a
harrelfe <fh@fharrell.com>
2022-05-12 07:35:19 -0500

    htmlView: added htmlViewx

--- htmlView.r
+++ htmlView.r
@@ -23,3 +23,35 @@ htmlView <- function(...) {
     viewer(fi)
   }
 }
+
+## htmlViewx is similar to htmlView except that an external viewer is
+## launched, and the first object is opened in a new window.
+## Subsequent objects are opened in a new tab in the last created
+## window.  Set options(vbrowser='command line to run browser')
+## to use a browser other than Vivaldi
+## Defaults to opening a new window for only the first object, and adding
+## tabs after that.
+## Set tab='all' to add even the first object to an existing window
+
+htmlViewx <- function(..., tab=c('notfirst', 'all', 'none')) {
+  tab <- match.arg(tab)
+  view <- getOption('vbrowser')
+  if(! length(view)) view <- 'vivaldi'
+  w <- list(...)
+  td <- tempdir()
+  if(! dir.exists(td)) dir.create(td)
+
+  i <- 0
+  for(u in w) {
+    i <- i + 1
+    fi <- paste0(tempfile(), '.html')
+    h  <- html(u)
+    writeLines(as.character(h), fi)
+    cmd <- paste0(view,
+                  switch(tab,
+                         all  = ' -new-tab',
+                         none = ' -new-window',
+                         notfirst = if(i == 1) ' -new-window' else ' -new-tab' ))
+    browseURL(fi, browser=cmd)
+  }
+}
