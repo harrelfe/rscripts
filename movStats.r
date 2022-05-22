e4697df3757f9a4afda0be341fced1e614186085
harrelfe <fh@fharrell.com>
2022-05-22 07:14:12 -0500

    movStats: fixed space='n'

--- movStats.r
+++ movStats.r
@@ -1,13 +1,13 @@
 ##' Moving Estimates Using Overlapping Fixed-width Windows
 ##' 
 ##' Function to compute moving averages and other statistics as a function
-##' of a continuous variable, possibly stratified by another variable
+##' of a continuous variable, possibly stratified by another variable.
 ##' Estimates are made by creating overlapping moving windows and
 ##' computing the statistics defined in the stat function for each window.
-##' By default estimates are made at the 10th smallest to the 10th largest
+##' The default method, `space='n'` creates varying-width intervals each having a sample size of `2*eps +1`, and the smooth estimates are made every `xinc` observations.  Outer intervals are not symmetric in sample size (but the mean x in those intervals will reflect that) unless `eps=10`, as outer intervals are centered at observations 10 and n - 10 + 1.  The mean x-variable within each windows is taken to represent that window.  If `trans` and `itrans` are given, x means are computed on the `trans(x)` scale and then `itrans`'d.  For `space='x'`, by default estimates are made on to the 10th smallest to the 10th largest
 ##' observed values of the x variable to avoid extrapolation and to
 ##' help getting the moving statistics off on an adequate start for
-##' the left tail.  Also by default the moving estimates are smoothed using `lowess`.
+##' the left tail.  Also by default the moving estimates are smoothed using `supsmu`.
 ##' When `melt=TRUE` you can feed the result into `ggplot` like this:
 ##' `ggplot(w, aes(x=age, y=crea, col=Type)) + geom_line() +`
 ##'   `facet_wrap(~ Statistic)`
@@ -19,9 +19,10 @@
 ##' @md
 ##' @param formula a formula with the analysis variable on the left and the x-variable on the right, following by an optional stratification variable
 ##' @param stat function of one argument that returns a named list of computed values.  Defaults to computing mean and quartiles + N except when y is binary in which case it computes moving proportions.  If y has two columns the default statistics are Kaplan-Meier estimates of cumulative incidence at a vector of `times`.
-##' @param eps tolerance for window (half width of window)
-##' @param xlim 2-vector of limits to evaluate (default is 10th to 10th)
-##' @param xinc  increment in x to evaluate stats, default is xlim range/100
+##' @param eps tolerance for window (half width of window).  For `space='x'` is in data units, otherwise is the sample size for half the window.
+##' @param varyeps applies to `space='n'` and causes a smaller `eps` to be used in strata with fewer than `` observations so as to arrive at three x points
+##' @param xinc  increment in x to evaluate stats, default is xlim range/100 for `space='x'`.  For `space='n'` `xinc` defaults to 5, meaning 5 observations.
+##' @param xlim 2-vector of limits to evaluate if `space='x'` (default is 10th to 10th)
 ##' @param times vector of times for evaluating one minus Kaplan-Meier estimates
 ##' @param tunits time units when `times` is given
 ##' @param msmooth set to `'smoothed'` or `'both'` to compute `lowess`-smooth moving estimates. `msmooth='both'` will display both.  `'raw'` will display only the moving statistics.  `msmooth='smoothed'` (the default) will display only he smoothed moving estimates.
@@ -44,11 +45,13 @@
 ##' @param melt  set to TRUE to melt data table and derive Type and Statistic
 ##' @param data: data.table or data.frame, default is calling frame
 ##' 
-movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
+movStats <- function(formula, stat=NULL, space=c('n', 'x'),
+                     eps =if(space=='n') 75, varyeps=FALSE,
+                     xinc=if(space=='n')  5, xlim=NULL,
                      times=NULL, tunits='year',
                      msmooth=c('smoothed', 'raw', 'both'),
                      tsmooth=c('supsmu', 'lowess'),
-                     bass=0, span=1/4, maxdim=6, penalty=NULL,
+                     bass=2, span=1/4, maxdim=6, penalty=NULL,
                      trans=function(x) x, itrans=function(x) x,
                      loess=FALSE,
                      ols=FALSE, qreg=FALSE, lrm=FALSE,
@@ -57,6 +60,7 @@ movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
                      data=environment(formula)) {
   require(data.table)
   if(ols || qreg || lrm) require(rms)
+  space   <- match.arg(space)
   msmooth <- match.arg(msmooth)
   tsmooth <- match.arg(tsmooth)
 
@@ -70,6 +74,8 @@ movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
     stop('when dependent variable has two columns you must specify times')
   if(sec && (loess || ols || qreg || lrm || orm))
     stop('loess, ols, qreg, lrm, orm do not apply when dependent variable has two columns')
+  if(varyeps && space == 'x')
+    stop('varyeps applies only to space="n"')
   
   if(sec) {
     require(survival)
@@ -84,10 +90,9 @@ movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
   i  <- is.na(X) | is.na(Y) | is.na(Y2) | is.na(By)
   if(any(i)) {
     i  <- ! i
-    X  <- X[i]
-    Y  <- Y[i]
+    X  <- X [i]
+    Y  <- Y [i]
     Y2 <- Y2[i]
-    Y  <- Y[i]
     By <- By[i]
   }
 
@@ -119,6 +124,12 @@ movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
                      N=length(y))
               }
 
+  statx <- function(y, y2, x) {
+    s <- if(sec) stat(y, y2) else stat(y)
+    if(space == 'n') s$.xmean. <- mean(x)
+    s
+  }
+
   R    <- NULL
   Xinc <- xinc
   
@@ -128,38 +139,69 @@ movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
       warning(paste('Stratum', by, 'has < 10 observations and is ignored'))
       next
     }
-    x  <- X[j]
-    y  <- Y[j]
+    x  <- X [j]
+    y  <- Y [j]
     y2 <- Y2[j]
     n  <- length(x)
 
-    xl <- xlim
-    if(! length(xl)) {
-      xs <- sort(x)
-      xl <- c(xs[10], xs[n - 10 + 1])
-      if(diff(xl) >= 25) xl <- round(xl)
-    }
-    xinc <- Xinc
-    if(! length(xinc)) xinc <- diff(xl) / 100.
-    xseq <- seq(xl[1], xl[2], by=xinc)
+    switch(space,
+           x = {
+             xl <- xlim
+             if(! length(xl)) {
+               xs <- sort(x)
+               xl <- c(xs[10], xs[n - 10 + 1])
+               if(diff(xl) >= 25) xl <- round(xl)
+             }
+             xinc <- Xinc
+             if(! length(xinc)) xinc <- diff(xl) / 100.
+             xseq <- seq(xl[1], xl[2], by=xinc)
+             xv   <- x
+           },
+           n = {
+             i    <- order(x)
+             x    <- x [i]
+             y    <- y [i]
+             y2   <- y2[i]
+             xseq <- seq(10, n - 10 + 1, by=xinc)
+             xv   <- 1 : n
+             } )
 
-    s <- data.table(x, y, y2)
-    a <- data.table(tx=xseq, key='tx')       # target xs for estimation
-    a[, .q(lo, hi) := .(tx - eps, tx + eps)] # define all windows
-    m <- a[s, on=.(lo <= x, hi >= x)]        # non-equi join
+    ep <- eps
+    if(space == 'n' && varyeps) {
+      ## Requirement to have >= 3 x-points; new eps=h
+      ## First estimate at observation min(xseq), last at max(xseq)
+      ## First window goes to min(xseq) + h on right
+      ## Last  window goes to max(xseq) - h on left
+      ## Need difference in these two to be >= 2*xinc
+      lowesteps <- floor((max(xseq) - min(xseq) - 2 * xinc) / 2.)
+      if(lowesteps < eps) ep <- lowesteps
+      }
+    
+    s <- data.table(x, y, y2, xv)
+    a <- data.table(tx=xseq, key='tx')     # target xs for estimation
+    a[, .q(lo, hi) := .(tx - ep, tx + ep)] # define all windows
+    m <- a[s, on=.(lo <= xv, hi >= xv)]    # non-equi join
     setkey(m, tx)
 
     ## Non-equi join adds observations tx=NA
     m <- m[! is.na(tx), ]
-    w <- if(sec) m[, stat(y, y2), by=tx]
-         else    m[, stat(y),     by=tx]
+    w <- m[, statx(y, y2, x), by=tx]
+
+    if(space == 'n') {
+      w[, tx := .xmean.]
+      w[, .xmean. := NULL]
+      }
 
     if(msmooth != 'raw') {
       computed <- setdiff(names(w), c('tx', 'N'))
       for(vv in computed) {
         smoothed <- switch(tsmooth,
-                           lowess = lowess(w[, tx], w[[vv]], f=span),
+                           lowess = lowess(w[, tx], w[[vv]], f   =span),
                            supsmu = supsmu(w[, tx], w[[vv]], bass=bass))
+        if(length(smoothed$x) < 2)
+          stop(paste0('Only 1 x point for stratum ', by,
+                      ' with ', n,
+                      ' observations. Consider specifying varyeps=TRUE.'))
         smfun <- function(x) approx(smoothed, xout=x)$y
         switch(msmooth,
                smoothed = {
@@ -174,7 +216,7 @@ movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
     }
 
     ## Also compute loess estimates
-    dat <- data.frame(x=xseq)
+    dat <- data.frame(x=w[, tx])
     if(loess) {
       np <- loess(y ~ x, data=s)
       pc <- predict(np, dat)
@@ -226,7 +268,7 @@ movStats <- function(formula, stat=NULL, eps, xlim=NULL, xinc=NULL,
            else
              hare(y, y2, x, maxdim=maxdim)
       for(ti in times) {
-        inc <- phare(ti, xseq, f)
+        inc <- phare(ti, dat$x, f)
         newname <- paste0('HARE ', ti, '-', tunits)
         w[, (newname) := inc]
       }
