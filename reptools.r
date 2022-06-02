7941efa8b2b916593f0291ca0c1e118088f98b4f
harrelfe <fh@fharrell.com>
2022-06-02 14:31:46 -0500

    reptools: seqFreq: new function

--- reptools.r
+++ reptools.r
@@ -828,3 +828,63 @@ asisOut <- function(...) {
   x <- if(length(x) > 1) paste0(unlist(x)) else x[[1]]
   knitr::asis_output(x)
   }
+
+
+seqFreq <- function(..., labels=NULL, noneNA=FALSE) {
+  d <- list(...)
+  k <- length(d)
+  if(length(labels)) nam <- labels
+  else {
+    s <- as.character(sys.call()[-1])[1 : k]
+    nam <- names(d)
+    if(! length(nam)) nam <- rep('', k)
+    nam <- ifelse(nam == '', s, nam)
+    }
+
+  ispos <- function(x) {
+    w <- if(is.logical(x)) x
+    else if(is.numeric(x)) x > 0
+    else tolower(as.character(x)) %in%
+           c('present', 'yes', 'y', 'true', 'positive', 'pos', '+')
+    w[is.na(x)] <- FALSE
+    1L * w
+  }
+
+  names(d) <- nam
+  x <- sapply(d, ispos)  # creates a matrix
+  # Count number of positives per observation
+  po   <- apply(x, 1, sum)
+  cond <- c(sum(po == 0), tabulate(po, nbins=k))
+
+  j <- integer(k)
+  w <- rep(0, nrow(x))
+  for(i in 1 : k) {
+    freqs <- apply(x, 2, sum)
+    if(all(freqs == 0)) break
+    imax  <- which.max(freqs)
+    j[i]  <- imax
+    w <- ifelse(w == 0 & x[, imax], imax, w)
+    x[x[, imax] == 1, ] <- 0
+  }
+j <- j[j != 0]
+x <- if(noneNA) factor(w, j, nam[j]) else
+                factor(w, c(0, j), c('none', nam[j]))
+attr(x, 'cond.per.obs') <- cond
+x
+}
+
+## Test
+if(FALSE) {
+set.seed(1)
+x1 <- runif(100)
+x2 <- sample(letters[1:5], 100, replace=TRUE)
+x3 <- sample(LETTERS[1:5], 100, replace=TRUE)
+sum(x1 > .5); sum(x2 == 'a'); sum(x3 == 'A') # 48 21 24
+sum(x1 <= .5 & x2 == 'a')             # 10
+sum(x1 <= .5 & x3 == 'A')             # 11
+sum(x1 <= .5 & x3 != 'A' & x2 == 'a') # 10
+sum(x1 <= .5 & x2 != 'a' & x3 != 'A') # 31
+w <- seqFreq(bigx1=x1 > .5, x2 == 'a', x3 == 'A')
+table(w)
+attributes(w)
+}
