06fd3265644e4b8e5b6abf674c24bffc157d7f17
harrelfe <fh@fharrell.com>
2022-10-16 08:37:05 -0500

    saveRfiles

--- saveRfiles.r
+++ saveRfiles.r
@@ -2,7 +2,7 @@ save_fts <- function(d, filebase=as.character(substitute(d)),
                 verbose=TRUE) {
     at <- keepHattrib(d)
     fd <- paste0(filebase, '.fst')
-    fa <- paste0(filebase, '.rds')
+    fa <- paste0(filebase, '-attr.rds')
     saveRDS(at, fa, compress='xz')
     write_fst(d, fd, compress=100)
     if(verbose) cat('Data saved in', fd, 'and attributes saved in', fa, '\n')
@@ -11,7 +11,7 @@ save_fts <- function(d, filebase=as.character(substitute(d)),
 
 retrieve_fts <- function(filebase, ...) {
     fd <- paste0(filebase, '.fst')
-    fa <- paste0(filebase, '.rds')
+    fa <- paste0(filebase, '-attr.rds')
     d  <- read_fst(fd, ...)
     at <- readRDS(fa)
     invisible(restoreHattrib(d, at))
