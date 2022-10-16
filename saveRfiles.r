99c1a307c023248918f48c5891494c2814df87a2
harrelfe <fh@fharrell.com>
2022-10-16 11:30:12 -0500

    saveRfiles: added documentation

--- saveRfiles.r
+++ saveRfiles.r
@@ -1,3 +1,20 @@
+# Functions to facilitate the use of the fst package
+# for efficiently storing and retrieving data frames/tables
+# by keeping Hmisc package attributes labels and units in
+# a separate RDS file when running save_fst() and by re-attaching
+# these attributes to variables when running retrieve_fst.
+#
+# d: a data frame/table
+# filebase: base file name, default is name of argument d
+# verbose: set to FALSE to make save_fst not print the names
+#   of the files created
+# ...: other arguments to read_fst, especially as.data.frame
+# For retrieve_fst the d argument's value is not used; d is
+# used to specify the base file name.  If you want to use a base
+# name that is not the object d name, put the first argument
+# to retrieve_fst in quotes.
+
+
 save_fst <- function(d, filebase=as.character(substitute(d)),
                 verbose=TRUE) {
     at <- keepHattrib(d)
@@ -9,7 +26,8 @@ save_fst <- function(d, filebase=as.character(substitute(d)),
     invisible()
 }
 
-retrieve_fst <- function(filebase, ...) {
+retrieve_fst <- function(d, ...) {
+    filebase <- as.character(substitute(d))
     fd <- paste0(filebase, '.fst')
     fa <- paste0(filebase, '-attr.rds')
     d  <- read_fst(fd, ...)
