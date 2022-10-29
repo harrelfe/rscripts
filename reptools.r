1c70dc9993ef2ef296e933b62c4f53957277de99
harrelfe <fh@fharrell.com>
2022-10-29 15:20:13 -0500

    reptools: spar: changed default mgp[1:2]

--- reptools.r
+++ reptools.r
@@ -1262,7 +1262,8 @@ spar <-
                    .5+top+.25*multi, .5+rt),
                  lwd = if(multi)1 else 1.75,
                  mgp = if(!axes) mgp=c(.75, .1, 0) else
-                 if(multi) c(1.5+0.45, .365, 0) else c(2.4-.4+0.45, 0.475, 0),
+                 if(multi) c(1.5+0.83,    0.365-0.03, 0) else
+                           c(2.4-.4+0.83, 0.475-0.03, 0),
                  tcl = if(multi)-0.25 else -0.4, xpd=FALSE, las=1,
                  bot=0, left=0, top=0, rt=0, ps=if(multi) 12 else 15,
                  mfrow=NULL, axes=TRUE, cex.lab=1.15, cex.axis=.8,
