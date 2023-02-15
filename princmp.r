879cbf96e8be4e0a34e6f73e6e6c41585d0a5b9f
harrelfe <fh@fharrell.com>
2023-02-14 18:57:57 -0600

    nhgh: used new princmp

--- princmp.r
+++ princmp.r
@@ -1,5 +1,5 @@
 princmp <- function(x, k=min(5, p), data, cor=TRUE, offset=0.8, col=1,
-                    adj=0, orig=TRUE) {
+                    adj=0, orig=TRUE, pl=TRUE) {
   g  <- princomp(x, cor=cor, data=data)
   co <- unclass(g$loadings)
   p  <- ncol(co)
@@ -12,14 +12,16 @@ princmp <- function(x, k=min(5, p), data, cor=TRUE, offset=0.8, col=1,
     co <- co / matrix(rep(sds, p), nrow=length(sds))
     print(round(co[, 1 : k], 5))
     }
-  
-  plot(g, type='lines', main='')
-  vars <- g$sdev^2
-  cumv <- cumsum(vars)/sum(vars)
-  p <- length(cumv)
-  text(1:p, vars + offset*par('cxy')[2],
-       as.character(round(cumv, 2)),
-       srt=45, adj=adj, cex=.65, xpd=NA, col=col)
+
+  if(pl) {
+    plot(g, type='lines', main='', npcs=k)
+    vars <- g$sdev^2
+    cumv <- cumsum(vars)/sum(vars)
+    p <- length(cumv)
+    text(1:k, vars[1:k] + offset*par('cxy')[2],
+         as.character(round(cumv[1:k], 2)),
+         srt=45, adj=adj, cex=.65, xpd=NA, col=col)
+    }
  
   # Use predict method with newdata to ensure that PCs NA when
   # orginal variables NA
