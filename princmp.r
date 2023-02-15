19151ebcb7c8e4c3322de0b282c0b5250d768944
harrelfe <fh@fharrell.com>
2023-02-15 14:16:38 -0600

    improved examples using princmp and omitting results=asis

--- princmp.r
+++ princmp.r
@@ -1,20 +1,37 @@
 princmp <- function(formula, data=environment(formula),
+                    method=c('regular', 'sparse'),
                     k=min(5, p), cor=TRUE, offset=0.8, col=1,
                     adj=0, orig=TRUE, pl=TRUE, sw=FALSE) {
+  method <- match.arg(method)
+  if(method == 'sparse')
+    if(! require(pcaPP))
+      stop('You must install the pcaPP package')
+  
   X  <- model.matrix.lm(formula, data)
   cat('Used', nrow(X), 'observations with no NAs out of', nrow(data), '\n')
   X  <- X[, -1]  # remove intercept
-  g  <- princomp(X, cor=cor)
+  g  <- switch(method,
+               regular = princomp(X, cor=cor),
+               sparse  = sPCAgrid(X, k=k, method='sd',
+                                  center=mean,
+                                  scale=if(cor) sd else function(x) 1.,
+                                  scores=TRUE, maxiter=10) )
+  
   co <- unclass(g$loadings)
   p  <- ncol(co)
-  cat('\nPC Coefficients of Standardized Variables\n')
-  print(round(co[, 1 : k], 4))
+  prz <- function(x, m) {
+    x <- format(round(x, m), zero.print=FALSE)
+    print(x, quote=FALSE)
+  }
+  type <- switch(method, regular='', sparse='Sparse ')
+  cat('\n', type, 'PC Coefficients of Standardized Variables\n', sep='')
+  prz(co[, 1 : k], 3)
   p <- ncol(co)
   if(orig) {
     sds <- g$scale
-    cat('\nPC Coefficients of Original Variables\n')
+    cat('\n', type, 'PC Coefficients of Original Variables\n', sep='')
     co <- co / matrix(rep(sds, p), nrow=length(sds))
-    print(round(co[, 1 : k], 5))
+    prz(co[, 1 : k], 5)
     }
 
   if(pl) {
@@ -31,12 +48,11 @@ princmp <- function(formula, data=environment(formula),
     if(! require(leaps)) stop('You must install the leaps package')
     for(j in 1 : k) {
       cat('\nStepwise Approximations to PC', j, '\n', sep='')
-      .pc. <- g$scores[, j]
       fchar <- capture.output(
-        f <- regsubsets(X, .pc., method='forward',
+        f <- regsubsets(X, g$scores[, j], method='forward',
                         nbest=1, nvmax=max(1, p - 1)))
       s <- summary(f)
-      w <- s$which[, -1]   # omit intercept
+      w <- s$which[, -1, drop=FALSE]   # omit intercept
       print(t(ifelse(w, '*', ' ')), quote=FALSE)
       cat('R2:', round(s$rsq, 3), '\n')
     }
