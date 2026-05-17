3c77df29db656682da94f96bba23b021a6ec4324
harrelfe <fh@fharrell.com>
2026-05-17 06:46:50 -0500

    added ggplot2 dependency

--- demoOLSqr.r
+++ demoOLSqr.r
@@ -8,6 +8,7 @@ n <- 20
 x <- runif(n)
 y <- abs(x - 0.5) + rnorm(n, mean=0, sd=.1)
 require(rms)
+require(ggplot2)
 mods <- list(y ~ x, y ~ pol(x, 2), y ~ pol(x, 3), y ~ pol(x, 4),
              y ~ pol(x, 5), y ~ rcs(x, 3), y ~ rcs(x, 4), y ~ rcs(x, 5))
 z <- NULL
